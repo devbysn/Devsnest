c424dae0c6bd7f6aea33312ac7c68515e02cdcc8
snverma1409 <snverma1409@gmail.com>
2021-07-11 15:45:09 +0530

    day17 completed

---
+++ Frontend/day16/task2/src/Card.js
@@ -0,0 +1,66 @@
+import React from "react";
+import CardElement from "./CardElement";
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="card">
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+      <CardElement />
+    </div>
+  );
+}
+
+export default Card;
