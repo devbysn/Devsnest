7aa8924c3537b74b0739d04e160961b0c924f64b
satya verma <52845970+devbysn@users.noreply.github.com>
2021-07-30 14:01:05 +0530

    master deleted

--- Frontend/Day08/main.js
+++
@@ -1,4 +0,0 @@
-function changeFunction(o) {
-  o.style.backgroundColor =
-    o.style.backgroundColor == "white" ? "#dd3927ce" : "white";
-}
--- Frontend/Day09/html css project/main.js
+++
@@ -1,8 +0,0 @@
-// function changeFunction(o) {
-//   o.style.backgroundColor =
-//     o.style.backgroundColor == "white" ? "#dd3927ce" : "white";
-// }
-
-const box = document.querySelectorAll("td");
-
-box.add;
